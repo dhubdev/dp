eab058cf5e264bf1f9fc7d52e2807e5f22e4af17
DHUB Dev <dhubdevs@gmail.com>
2024-09-20 00:11:22 +0100

    services page = done

---
+++ src/components/svelte/ServiceList.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { services } from "$lib/modules/constants";
+  import Service from "./cards/Service.svelte";
+</script>
+
+<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 items-stretch">
+  {#each services as service, i}
+    <Service {service} />
+  {/each}
+</div>
