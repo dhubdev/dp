c2aa8f29df9a667517f415497ba389756ef526f0
DHUB Dev <dhubdevs@gmail.com>
2024-09-20 11:24:00 +0100

    move

---
+++ src/components/svelte/ContactBtns.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import Button from "../ui/button/button.svelte";
+  import WhatsApp from "./icons/WhatsApp.svelte";
+</script>
+
+<div class="flex items-center justify-center gap-4">
+  <Button variant="outline" href="/contact">
+    Contact Us
+  </Button>
+  <Button size="icon" href="https://api.whatsapp.com/send?phone=+447930739927&text=Hello">
+    <WhatsApp />
+  </Button>
+</div>
