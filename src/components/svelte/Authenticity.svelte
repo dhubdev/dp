de86bc14cc5e200db1db5150815d6e7d474ca620
Jumia <dhubdevs@gmail.com>
2024-11-21 13:15:52 +0100

    push

---
+++ src/components/svelte/Authenticity.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  interface iIcon {
+    name: string;
+    src: string;
+  }
+
+  let icons: iIcon[] = [
+    {
+      name: "BICSc",
+      src: "/authenticity/bicsc-logo.webp",
+    },
+    {
+      name: "100% Customer Satisfaction Guaranteed",
+      src: "/authenticity/customer-satisfaction-guaranteed.webp",
+    },
+    {
+      name: "Eco-Friendly",
+      src: "/authenticity/eco-friendly.webp",
+    },
+    {
+      name: "Universal Recycling",
+      src: "/authenticity/universal-recycling.webp",
+    },
+  ];
+</script>
+
+<div class="grid grid-cols-2 md:grid-cols-4 gap-4 place-items-center">
+  {#each icons as { name, src }, i}
+    <div class="w-full rounded-lg p-4 dark:bg-white">
+      <img {src} alt={name} class="aspect-auto" />
+    </div>
+  {/each}
+</div>
