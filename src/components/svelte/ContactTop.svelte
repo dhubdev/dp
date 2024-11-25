1cfa3f09adcc9fe836d1aeda58a01ec4d5df007a
Jumia <dhubdevs@gmail.com>
2024-11-25 12:32:18 +0100

    Update ContactTop.svelte

--- src/components/svelte/ContactTop.svelte
+++ src/components/svelte/ContactTop.svelte
@@ -5,33 +5,33 @@
   import { Button } from "../ui/button";
 </script>
 
-<div class="h-10 bg-primary hidden md:block">
+<div class="h-10 bg-primary block">
   <div class="center flex items-center justify-between !py-0 h-full">
     <Button
       variant="ghost"
       href="https://www.google.com/maps/place/5+Union+St,+Ardwick,+Greater+M12+4JD/@53.4728546,-2.2234096,17z/data=!3m1!4b1!4m6!3m5!1s0x487bb1983e4228d5:0x1fa06f2556e8f6ab!8m2!3d53.4728546!4d-2.2208347!16s%2Fg%2F11jzpm82kf?entry=ttu&g_ep=EgoyMDI0MDgyNi4wIKXMDSoASAFQAw%3D%3D"
-      class="flex items-center gap-2 !p-0 h-full !px-2 text-white"
+      class="flex items-center gap-2 !p-0 h-full !px-2 text-white hover:text-white"
     >
-      <Map />
-      <span>5 Union Street, City View House, Manchester M12 4JD UK</span>
+      <Map class="hover:text-white" />
+      <span class="hidden md:inline-block">5 Union Street, City View House, Manchester M12 4JD UK</span>
     </Button>
 
     <div class="flex items-center gap-2">
       <Button
         href="tel:+447930739927"
-        class="flex items-center gap-2 !p-0 h-full !px-2 text-white"
+        class="flex items-center gap-2 !p-0 h-full !px-2 text-white hover:text-white"
         variant="ghost"
       >
-        <Phone />
-        <span>+44 793 073 9927</span>
+        <Phone class="hover:text-white" />
+        <span class="hidden md:inline-block">+44 793 073 9927</span>
       </Button>
       <Button
         variant="ghost"
         href="mailto:enquiries@dhubproperty.co.uk?subject=DHUB%20Property%20Inquiry&body=Hello"
-        class="flex items-center gap-2 !p-0 h-full !px-2 text-white"
+        class="flex items-center gap-2 !p-0 h-full !px-2 text-white hover:text-white"
       >
-        <Email />
-        <span>enquiries@dhubproperty.co.uk</span>
+        <Email class="hover:text-white" />
+        <span class="hidden md:inline-block">enquiries@dhubproperty.co.uk</span>
       </Button>
     </div>
   </div>
