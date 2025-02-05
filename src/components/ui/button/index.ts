8689413a74818e0fd0d284fb9da48ac80b550bff
Jumia <dhubdevs@gmail.com>
2025-02-05 10:59:10 +0100

    push

--- src/components/ui/button/index.ts
+++ src/components/ui/button/index.ts
@@ -6,7 +6,7 @@ const buttonVariants = tv({
 	base: "ring-offset-background focus-visible:ring-ring inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 rounded-full",
 	variants: {
 		variant: {
-			default: "bg-primary text-primary-foreground dark:bg-white dark:text-primary font-bold hover:bg-primary/90 dark:font-bold",
+			default: "bg-primary text-primary-foreground font-bold hover:bg-primary/90",
 			destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/90",
 			outline:
 				"border-input bg-background hover:bg-accent hover:text-accent-foreground border",
