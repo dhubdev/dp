2ecce79167f93806447bf69be7a2f659b23fde6e
Jumia <dhubdevs@gmail.com>
2025-02-17 12:55:00 +0100

    Update SelfSponsorshipComponent.svelte

---
+++ src/components/svelte/SelfSponsorshipComponent.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+
+</script>
+
+<div class="center prose dark:prose-invert">
+  <blockquote>
+    Our Self-Sponsorship Visa Route UK Lawyers Will Review Your Qualifications, Skills and Experience to Determine the Best Approach for a Successful Application
+  </blockquote>
+</div>
