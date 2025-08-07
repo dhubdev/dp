0536bf9ac0d0e11c7f507a617e6491872b0f5b22
TnTSpace <dhubdevs@gmail.com>
2025-08-07 07:52:09 +0100

    Update Logo.svelte

--- src/components/svelte/icons/Logo.svelte
+++ src/components/svelte/icons/Logo.svelte
@@ -3,10 +3,10 @@
   import { Button } from "$lib/components/ui/button";
 </script>
 
-<Button class="flex items-center gap-2 no-underline !p-0" variant="ghost" href="/">
+<a class="flex items-center gap-2 no-underline !p-0" variant="ghost" href="/">
   <LogoIcon />
   <div class="flex flex-col leading-[1] text-primary dark:text-white">
     <p class="font-bold">DHUB</p>
     <span class="font-bold text-xxs">PROPERTY</span>
   </div>
-</Button>
+</a>
